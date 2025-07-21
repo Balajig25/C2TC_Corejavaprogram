034ce9ac30be23d5a2fc3e130cc2229766d8ff61
VIJI <VIJI@Balaji>
2025-07-21 14:33:28 +0530

    day programs

--- C2TC_Core javaprograms/src/day1/basics/SampleDemo.java
+++ C2TC_Core javaprograms/src/day1/basics/SampleDemo.java
@@ -1,10 +1,17 @@
 package day1.basics;
 
 public class SampleDemo {
-
+	static
+	{
+	System.out.println("ststic block 2");	
+	}
 	public static void main(String[] args) {
-		System.out.println("welcome to ...."); 
-
+		System.out.println("HELLO");
+		System.out.println("next line");
+	}
+	static
+	{
+	System.out.println("ststic block 1");	
 	}
 
 }
---
+++ C2TC_Core javaprograms/src/day1/basics/sampledemo1.java
@@ -0,0 +1,23 @@
+package day1.basics;
+
+public class sampledemo1 {
+	public static void main()
+	{
+		System.out.println("welcome main 1");
+	}
+	public static void main(char c[])
+	{
+		System.out.println("welcome main 2");
+	}
+	public void main(String ar)
+	{
+		System.out.println("welcome main 3")
+	}
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
